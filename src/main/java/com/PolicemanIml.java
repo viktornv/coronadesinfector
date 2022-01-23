c86fd6c2a331f25e0297fba1b8360f65508c4582
Viktor <viktornv@gmail.com>
2022-01-23 09:59:42 +0300

    Revert "second commit"
    This reverts commit 0f7601780e326e18deb841887d53df096635f279.

--- src/main/java/com/PolicemanIml.java
+++ src/main/java/com/PolicemanIml.java
@@ -3,6 +3,6 @@ package com;
 public class PolicemanIml implements Policman {
     @Override
     public void makePeopleLeaveRoom() {
-        System.out.println("пиф паф, я буду стрелять");
+        System.out.println("пиф паф");
     }
 }
